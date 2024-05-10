ffe18a196e52466899bec254244ddfd879cd5ac4
Manu Phatak <manu@phatak.io>
2024-05-10 11:29:28 -0500

    Initial commit

---
+++ apps/nx-jest-bug/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-jest-bug',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-jest-bug'
+};
